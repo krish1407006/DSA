20d8f54149d66e07456ff02e449a1305a13051a2
krish1407006 <krish14072006@gmail.com>
2026-06-03 19:05:36 +0530

    day3

--- day3.cpp
+++ day3.cpp
@@ -1,24 +1,39 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-    int marks;
-    cout << "Enter your marks: ";
-    cin >> marks;
+// int main() {
+//     int marks;
+//     cout << "Enter your marks: ";
+//     cin >> marks;
 
-    if(marks >=90 ) {
-        cout << "You have scored an A grade.";
-    }
-    else if (marks >=80 && marks <90) {
-        cout << "You have scored a B grade.";
-    }
-    else if (marks >=70 && marks <80) {
-        cout << "You have scored a C grade.";
-    }
-    else if (marks >=60 && marks <70) {
-        cout << "You have scored a D grade.";
-    }
-    else {
-        cout << "You have scored an F grade.";
+//     if(marks >=90 ) {
+//         cout << "You have scored an A grade.";
+//     }
+//     else if (marks >=80 && marks <90) {
+//         cout << "You have scored a B grade.";
+//     }
+//     else if (marks >=70 && marks <80) {
+//         cout << "You have scored a C grade.";
+//     }
+//     else if (marks >=60 && marks <70) {
+//         cout << "You have scored a D grade.";
+//     }
+//     else {
+//         cout << "You have scored an F grade.";
+//     }
+// }
+
+int main() {
+    char ch;
+    cout << "  Enter a character : ";
+    cin>> ch;
+
+    if ((ch >='a' && ch<='z')) {
+    cout << "lowercase";
+}
+    else if (ch >='A' && ch<='Z') {
+        cout << "uppercase";
     }
 }
+
+
