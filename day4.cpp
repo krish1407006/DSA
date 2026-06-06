614cfd5d80a6f42b7e3ebccd152611e8478b0c52
krish1407006 <krish14072006@gmail.com>
2026-06-06 16:02:54 +0530

    day4

---
+++ day4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+
+    // while (num<10) {
+    //     cout << num << " ";
+    //     num++;
+// }
+
+//do while loop
+
+int n = 10;
+int i = 1;
+  do {
+    cout << i << ' ';
+    i++;
+  }
+  while 
+      (i<=n);
+  cout << endl;
+  return 0;
+  
+}
+
