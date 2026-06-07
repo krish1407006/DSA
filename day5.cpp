17eba557333943c1529264f5fd6dda7e3e3eacb0
krish1407006 <krish14072006@gmail.com>
2026-06-07 17:36:30 +0530

    day5

--- day5.cpp
+++ day5.cpp
@@ -14,27 +14,55 @@ int main () {
 
 
 
-int k = 5;
+// int k = 5;
  
-for (int i=1; i<=k; i++) {
-    for (int j=1; j<=k ; j++) {
-        cout <<" & ";
-    }
-    cout << endl;
-}
+// for (int i=1; i<=k; i++) {
+//     for (int j=1; j<=k ; j++) {
+//         cout <<" & ";
+//     }
+//     cout << endl;
+// }
 
 
 
 
-int n = 5;
+// int n = 5;
 
-   for (int i=1; i<=n; i++) {
-    char ch= 'A';
-    for (int j=1;  j<=n; j++) {
-    cout << ch << "  ";
-    ch = ch + 1;
+//    for (int i=1; i<=n; i++) {
+//     char ch= 'A';
+//     for (int j=1;  j<=n; j++) {
+//     cout << ch << "  ";
+//     ch = ch + 1;
         
-    }
-    cout << endl;}
-    return 0;
-}
+//     }
+//     cout << endl;}
+//     return 0;
+// }
+
+
+
+// int n =5;
+
+// int num = 1;
+
+// for (int i=1; i<n; i++) {
+//     for (int j=1; j<=n; j++) {
+//         cout << num << "  ";
+//         num++;
+//     }
+//     cout << endl;
+// }
+// }
+
+
+// int n = 5;
+
+// int num = 1;
+// for (int i=0; i<=n; i++) {
+//     for (int j=1; j<=i+1; j++) {
+//         cout << i+1;
+//         num++;
+//     }
+//     cout << endl;
+// }
+// }
