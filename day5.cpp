93094f05aaac15046a6439f7d364911074235711
krish1407006 <krish14072006@gmail.com>
2026-06-07 16:53:01 +0530

    day5

--- day5.cpp
+++ day5.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 
 int main () {
-int n ;
-    for (int i = 1; i <=5; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << " * " ;
-        }
-        cout << endl;
+// int n ;
+//     for (int i = 1; i <=5; i++) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << " * " ;
+//         }
+//         cout << endl;
     
-    return 0;
+//     return 0;
+// }
+
+
+
+int n = 5;
+ 
+for (int i=1; i<=n; i++) {
+    for (int j=1; j<=n ; j++) {
+        cout <<" & ";
+    }
+    cout << endl;
+}
+return 0;
 }
