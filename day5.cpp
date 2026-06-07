e53553f18fa16b943ba84389d4e60697475456b9
krish1407006 <krish14072006@gmail.com>
2026-06-07 17:10:09 +0530

    day5

--- day5.cpp
+++ day5.cpp
@@ -14,13 +14,27 @@ int main () {
 
 
 
-int n = 5;
+// int n = 5;
  
-for (int i=1; i<=n; i++) {
-    for (int j=1; j<=n ; j++) {
-        cout <<" & ";
-    }
-    cout << endl;
-}
-return 0;
+// for (int i=1; i<=n; i++) {
+//     for (int j=1; j<=n ; j++) {
+//         cout <<" & ";
+//     }
+//     cout << endl;
+// }
+// return 0;
+// }
+
+
+
+int n = 5;
+
+for (int i =1; i<n; i++) {
+    char ch= 'A';
+    for (int j=1;  j<n; j++) {
+        cout << ch << " ";
+        ch = ch + 1;
+        
+    }}
+    return 0;
 }
