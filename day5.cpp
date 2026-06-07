d850f93b49557ace59b41a23094fba91141cc392
krish1407006 <krish14072006@gmail.com>
2026-06-07 17:42:01 +0530

    day5

--- day5.cpp
+++ day5.cpp
@@ -66,3 +66,14 @@ int main () {
 //     cout << endl;
 // }
 // }
+
+
+int n =5;
+
+for (int i=1; i<=n; i++) {
+    for (int j=1; j<=i; j++) {
+        cout << j << "  ";
+    }
+    cout << endl;
+}
+}
