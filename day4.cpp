e9d4f5c0d9606b00924a5413d41a117151494971
krish1407006 <krish14072006@gmail.com>
2026-06-07 11:17:40 +0530

    day4

--- day4.cpp
+++ day4.cpp
@@ -13,16 +13,32 @@ int main () {
 
 //do while loop
 
-int n = 10;
-int i = 1;
-  do {
-    cout << i << ' ';
-    i++;
-  }
-  while 
-      (i<=n);
-  cout << endl;
-  return 0;
+// int n = 10;
+// int i = 1;
+//   do {
+//     cout << i << ' ';
+//     i++;
+//   }
+//   while 
+//       (i<=n);
+//   cout << endl;
+//   return 0;
   
-}
+int num = 7;
+bool isPrime = true;
 
+ 
+    for (int i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    
+        if (isPrime == true) {
+            cout << num << " is a prime number." << endl;
+        } else {
+            cout << num << " is not a prime number." << endl;
+        }
+    }
+}
