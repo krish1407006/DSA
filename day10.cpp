901c1cfa9c73291218592bd431871fa1eed6ec61
krish1407006 <krish14072006@gmail.com>
2026-06-17 19:30:44 +0530

    day10

--- day10.cpp
+++ day10.cpp
@@ -1,25 +1,44 @@
 #include<iostream>
 using namespace std;
 
-int linearSearch(int arr[], int size, int target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i; 
-        }
-    }
-    return -1;
-}
+// int linearSearch(int arr[], int size, int target) {
+//     for (int i = 0; i < size; i++) {
+//         if (arr[i] == target) {
+//             return i; 
+//         }
+//     }
+//     return -1;
+// }
+// int main () {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = 5;
+//    cout << "Enter the element to search: ";
+//     int target;
+//     cin >> target;
+//     int result = linearSearch(arr, size, target);
+//     if (result != -1) {
+//         cout << "Element found at index: " << result << endl;
+//     } else {
+//         cout << "Element not found" << endl;
+//     }
+//     return 0;
+// }
+
+
 int main () {
+
     int arr[] = {1, 2, 3, 4, 5};
     int size = 5;
-   cout << "Enter the element to search: ";
-    int target;
-    cin >> target;
-    int result = linearSearch(arr, size, target);
-    if (result != -1) {
-        cout << "Element found at index: " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
+
+    for (int i = 0; i < size / 2; i++) {
+        swap(arr[i], arr[size - i - 1]);
     }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
     return 0;
 }
