e3a7b42b75a5b4419f1e09457a033006ae32a0ab
krish1407006 <krish14072006@gmail.com>
2026-06-17 17:51:00 +0530

    day10

---
+++ day10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1;
+}
+int main () {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = 5;
+   cout << "Enter the element to search: ";
+    int target;
+    cin >> target;
+    int result = linearSearch(arr, size, target);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    return 0;
+}
