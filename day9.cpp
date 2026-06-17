0d59be43c26004d54f92fca8d44590221aa74121
krish1407006 <krish14072006@gmail.com>
2026-06-17 14:53:50 +0530

    day9

--- day9.cpp
+++ day9.cpp
@@ -3,14 +3,29 @@ using namespace std;
 
 int main() {
 
-    int arr[5];
+//     int arr[5];
 
-    int marks [5] = { 90, 80, 70, 60, 50 };
-   cout << "Size of marks array: " << sizeof(marks) << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << marks[i] << " ";
-    }
-    cout << sizeof(marks) << endl;
+//     int marks [5] = { 90, 80, 70, 60, 50 };
+//    cout << "Size of marks array: " << sizeof(marks) << endl;
+//     for (int i = 0; i < 5; i++) {
+//         cout << marks[i] << " ";
+//     }
+//     cout << sizeof(marks) << endl;
 
+
+
+int nums[] = { 10, 20, -15, -40, 50 };
+
+int size = 5;
+
+int smallest = INT_MAX;
+for (int i = 0; i < size; i++) {
+if (nums[i] < smallest) {
+    smallest = nums[i];
+
+}
+}
+cout << "Smallest number so far: " << smallest << endl;
     return 0;
+
 }
