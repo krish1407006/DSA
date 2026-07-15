994e8daea8502de20c99af2a30d9a36edcc8d1c0
krish1407006 <krish14072006@gmail.com>
2026-07-15 22:47:25 +0530

    day13

--- day13.cpp
+++ day13.cpp
@@ -6,16 +6,31 @@ int main() {
     int n = 5;
     int arr[5] = {1, 2, 3, 4, 5};
 
-    for(int st=0; st<n; st++){
-        for(int end=st; end<n; end++) {
-            for(int i=st; i<=end; i++){
-                cout<<arr[i];
-            }
-            cout<< " ";
-        }
-            cout<<endl;
+    // for(int st=0; st<n; st++){
+    //     for(int end=st; end<n; end++) {
+    //         for(int i=st; i<=end; i++){
+    //             cout<<arr[i];
+        //     }
+        //     cout<< " ";
+        // }
+        //     cout<<endl;
         
+    // }
+
+    int maxsum = INT_MIN;
+    for(int st=0; st<n; st++) {
+        int currentsum = 0;
+        for(int end=st; end<n; end++) {
+            currentsum = currentsum + arr[end];
+            if(currentsum > maxsum) {
+                maxsum = currentsum;
+            }
+          
+            cout<<maxsum<<" ";
+            cout<<endl;
+        }
     }
+
 
     
 }
