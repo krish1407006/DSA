9ffc71e60856f3d12df827329f932fdde35f4608
krish1407006 <krish14072006@gmail.com>
2026-07-15 22:38:11 +0530

    day13

--- day13.cpp
+++ day13.cpp
@@ -9,10 +9,12 @@ int main() {
     for(int st=0; st<n; st++){
         for(int end=st; end<n; end++) {
             for(int i=st; i<=end; i++){
-                cout<<arr[i]<<" ";
+                cout<<arr[i];
             }
-            cout<<endl;
+            cout<< " ";
         }
+            cout<<endl;
+        
     }
 
     
